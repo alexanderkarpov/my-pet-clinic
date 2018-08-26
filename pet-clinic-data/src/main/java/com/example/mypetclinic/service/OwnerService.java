aa5373143b22063a46f1322a64ffc54392fe28f8
Alexander Karpov <alexander.karpoff@gmail.com>
2018-08-26 12:53:28 +0300

    crud interface

--- pet-clinic-data/src/main/java/com/example/mypetclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/com/example/mypetclinic/service/OwnerService.java
@@ -2,15 +2,8 @@ package com.example.mypetclinic.service;
 
 import com.example.mypetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/example/mypetclinic/service/PetService.java
+++ pet-clinic-data/src/main/java/com/example/mypetclinic/service/PetService.java
@@ -2,13 +2,5 @@ package com.example.mypetclinic.service;
 
 import com.example.mypetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
